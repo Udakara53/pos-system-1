d4c1863f2d5abe9ac84d4887cbccf9a163a0e8f6
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-07 13:09:56 +0530

    loginOnAction button's function is set.

--- src/lk/icet/pos/control/LoginFormController.java
+++ src/lk/icet/pos/control/LoginFormController.java
@@ -1,11 +1,13 @@
 package lk.icet.pos.control;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lk.icet.pos.db.Database;
 import lk.icet.pos.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class LoginFormController {
     public AnchorPane loginFormContext;
@@ -17,6 +19,17 @@ public class LoginFormController {
     }
 
     public void loginOnAction(ActionEvent actionEvent) {
-
+        User selectedUser = Database.users.stream()
+                .filter(user -> user.getUsername().equals(txtUserName.getText()))
+                .findFirst().orElse(null);
+        if(selectedUser!=null){
+            if(BCrypt.checkpw(pwd.getText(),selectedUser.getPassword())){
+                System.out.println("user logged");
+            }else{
+                System.out.println("wrong password");
+            }
+        }else{
+            new Alert(Alert.AlertType.WARNING,"User name not found!").show();
+        }
     }
 }
