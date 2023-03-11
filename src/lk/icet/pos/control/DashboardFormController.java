2140457919ce6168b316f15dcf1398aba49b8fe8
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-11 19:13:13 +0530

    set onAction to the logout button back to the loginForm

--- src/lk/icet/pos/control/DashboardFormController.java
+++ src/lk/icet/pos/control/DashboardFormController.java
@@ -1,4 +1,22 @@
 package lk.icet.pos.control;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+    public AnchorPane dashboardContext;
+
+    public void logoutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) dashboardContext.getScene().getWindow();
+        stage.setScene(
+                new Scene(FXMLLoader.load(getClass().getResource("../view/LoginForm.fxml")))
+        );
+        new Alert(Alert.AlertType.WARNING,"Logged Out!").show();
+    }
 }
