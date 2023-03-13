f7c4b60744aac5d609c5e8ae202728d28927999f
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-13 19:57:49 +0530

    setup a combobox function. when select show the relavent data in textfields,

--- src/lk/icet/pos/control/PlaceOrderFormController.java
+++ src/lk/icet/pos/control/PlaceOrderFormController.java
@@ -1,4 +1,40 @@
 package lk.icet.pos.control;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import lk.icet.pos.db.Database;
+import lk.icet.pos.entity.Customer;
+
 public class PlaceOrderFormController {
+    public ComboBox cmbCustomerId;
+    public TextField txtCustomerName;
+    public TextField txtCustomerAddress;
+    public TextField txtSalary;
+
+    public void initialize(){
+        loadCustomerIds();
+        cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue!=null){
+                setCustomerData((String) newValue);
+            }
+        });
+
+    }
+    private void setCustomerData(String id){
+        Customer customer = Database.customers.stream().filter(e -> e.getId().equals(id)).findFirst().orElse(null);
+        if (customer!=null){
+            txtCustomerName.setText(customer.getName());
+            txtCustomerAddress.setText(customer.getAddress());
+            txtSalary.setText(String.valueOf(customer.getSalary()));
+        }else{
+            new Alert(Alert.AlertType.WARNING,"Not Found").show();
+        }
+    }
+
+    private void loadCustomerIds() {
+        for (Customer data: Database.customers){
+            cmbCustomerId.getItems().add(data.getId());
+        }
+    }
 }
--- src/lk/icet/pos/db/Database.java
+++ src/lk/icet/pos/db/Database.java
@@ -19,6 +19,10 @@ public class Database {
         users.add(new User("anna",encryptPassword("1234")));
         users.add(new User("linda",encryptPassword("1234")));
 
+        Customer customer1 = new Customer("C001","Nimal","Panadura",25000);
+        Customer customer2 = new Customer("C002","Lasath","Galle",28000);
+        Customer customer3 = new Customer("C003","Kamal","Ratnapura",35000);
+
         Item item1 = new Item("D-001","Description 1",25,2500);
         Item item2 = new Item("D-002","Description 2",25,4568);
         Item item3 = new Item("D-003","Description 3",20,3000);
@@ -26,6 +30,9 @@ public class Database {
 
         items.addAll(
                 Arrays.asList(item1,item2,item3,item4)
+        );
+        customers.addAll(
+                Arrays.asList(customer1,customer2,customer3)
         );
         //System.out.println(users);
     }
