a87af610d2701f56a4be79f77aae24dfc7fc1bb7
Tharindu Udakara <udakaratharindu@gmail.com>
2023-03-19 02:35:20 +0530

    orderDetailsForm is implemented. table view

--- src/lk/icet/pos/control/OrderDetailsFormController.java
+++ src/lk/icet/pos/control/OrderDetailsFormController.java
@@ -1,14 +1,20 @@
 package lk.icet.pos.control;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import lk.icet.pos.db.Database;
 import lk.icet.pos.entity.Order;
+import lk.icet.pos.entity.OrderDetails;
+import lk.icet.pos.view.tm.OrderDetailsTM;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class OrderDetailsFormController {
@@ -17,11 +23,20 @@ public class OrderDetailsFormController {
     public TextField txtId;
     public TextField txtName;
     public TextField txtDate;
-    public TableView<?> tblOrderDetail;
-    public TableColumn<?, ?> colItem;
-    public TableColumn<?, ?> colDescription;
-    public TableColumn<?, ?> colQty;
-    public TableColumn<?, ?> colUnitPrice;
+    public TableView<OrderDetailsTM> tblOrderDetail;
+    public TableColumn<OrderDetailsTM, String> colItem;
+    public TableColumn<OrderDetailsTM, String> colDescription;
+    public TableColumn<OrderDetailsTM, Integer> colQty;
+    public TableColumn<OrderDetailsTM, Double> colUnitPrice;
+
+    public void initialize(){
+        colItem.setCellValueFactory(new PropertyValueFactory<>("code"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        colQty.setCellValueFactory(new PropertyValueFactory<>("qty"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+
+
+    }
 
 
     public void setOrder(String orderId){
@@ -36,6 +51,31 @@ public class OrderDetailsFormController {
                 .findFirst().get().getName());
         txtDate.setText(new SimpleDateFormat("yyyy-MM-dd").format(order.get().getDate()));
         txtCost.setText(String.valueOf(order.get().getTotal()));
+        loadTable(orderId);
+    }
+
+    private void loadTable(String orderId) {
+        //get the order
+        //extract items
+        //load table-===> item code description
+        Optional<Order> selectOrder = Database.orders.stream().filter(e -> e.getOrderId().equals(orderId)).findFirst();
+        if (!selectOrder.isPresent()){
+            new Alert(Alert.AlertType.WARNING,"Not Found").show();
+            return;
+        }
+        ArrayList<OrderDetails> items = selectOrder.get().getProducts();
+        ObservableList<OrderDetailsTM> tmList = FXCollections.observableArrayList();
+        for (OrderDetails d: items
+             ) {
+            tmList.add(
+                    new OrderDetailsTM(
+                            d.getCode(),
+                            Database.items.stream().filter(e->e.getCode().equals(d.getCode())).findFirst().get().getDescription(),
+                            d.getQty(),
+                            d.getUnitPrice())
+            );
+        }
+        tblOrderDetail.setItems(tmList);
     }
 
 
